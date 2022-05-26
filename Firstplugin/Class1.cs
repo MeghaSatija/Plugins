a17d4b840055c66c6e212c9cc347c8cd714f7b78
hp <hp@DESKTOP-FG5DQK6>
2022-05-26 17:01:04 +0530

    Add project files.

---
+++ Firstplugin/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+
+namespace Firstplugin
+{
+    public class Class1 : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));//
+
+            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+            ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            try
+            {
+                tracingService.Trace("Tracing Start");
+                if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+                {
+                    Entity targetentity = (Entity)context.InputParameters["Target"];
+                    targetentity["telephone1"] = "123456789";
+                    service.Update(targetentity);
+                }
+            }
+            catch (Exception E) { throw new InvalidPluginExecutionException(E.Message); }
+        }
+    }
+}
+
